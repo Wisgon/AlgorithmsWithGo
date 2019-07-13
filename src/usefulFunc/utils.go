0836ecee3558787d07cd5f01b589fbd94a299857
zhilong <286142377@qq.com>
2019-07-13 20:39:56 +0800

    deal with error

--- src/usefulFunc/utils.go
+++ src/usefulFunc/utils.go
@@ -1,9 +1,26 @@
 package usefulFunc
 
+import (
+	"log"
+)
+
 func MatrixesCanMultiply(A [][]int, B [][]int) bool {
 	if len(A[0]) == len(B) {
 		return true
 	} else {
 		return false
+	}
+}
+
+func DealWithError(how string, err error) {
+	if err != nil {
+		switch how {
+		case "logln":
+			log.Fatalln(err)
+		case "panic":
+			panic(err)
+		default:
+			panic("Unknow method to deal with error!")
+		}
 	}
 }
